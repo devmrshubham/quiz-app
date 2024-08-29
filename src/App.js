c90b607c6bf9f68081d55f656702735f4e72ebec
shubham <sd053366@gmail.com>
2024-08-29 06:56:02 +0530

    css file and top heading add

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Quiz from './Component/Quiz'
 
 const App = () => {
   return (
-    <>
-      <Quiz/>
+    <><br /><br />
+      <h1 style={{textAlign:"center"}}>Quiz App</h1>
+      <Quiz />
     </>
   )
 }
